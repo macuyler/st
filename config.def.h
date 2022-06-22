aaeb61953e7ce1d7dd72f31d01af4bde40ace68f
Macuyler Dunn <macuyler@protonmail.com>
2022-06-22 15:00:08 -0500

    Update config
    - Set default rows to 80
    - Set default cols to 118

--- config.def.h
+++ config.def.h
@@ -147,8 +147,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 118;
+static unsigned int rows = 80;
 
 /*
  * Default colour and shape of the mouse cursor
